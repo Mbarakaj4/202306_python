7c3b09011b1e45d1ee2f50ce610908091d133e66
Mbarakaj4 <113691935+Mbarakaj4@users.noreply.github.com>
2023-07-19 16:08:33 -0400

    Update DojoPets
    Update Ninja.py and Pet.py functionalities
    Add a new sublass "Cat" to Pets
    Update DojoPets.py

--- Practice/CodingDojo-DojoPets/DojoPets.py
+++ Practice/CodingDojo-DojoPets/DojoPets.py
@@ -1,9 +1,18 @@
 from Ninja import Ninja
-from Pet import Pets
+import Pet
 
-cat = Pets("catto", "cat", "generic food", "100%", "full")
-blue_ninja = Ninja("blue","ninja","generic food", cat)
+cat = Pet.Pets("catto", "cat", "generic food", "100%", "full")
+blue_ninja = Ninja("blue","ninja","generic treats", "generic food", cat)
 
 blue_ninja.feed()
 blue_ninja.walk()
 blue_ninja.shower()
+blue_ninja.pet.noise()
+
+gatete = Pet.Cat("Michiatto", "generic food", "100%", "full")
+red_ninja = Ninja("red","ninja","generic treats", "generic food", gatete)
+
+red_ninja.feed()
+red_ninja.walk()
+red_ninja.shower()
+red_ninja.pet.noise()
--- Practice/CodingDojo-DojoPets/Pet.py
+++ Practice/CodingDojo-DojoPets/Pet.py
@@ -7,17 +7,31 @@ class Pets:
         self.energy = energy
     
 
-    def sleep() -> None:
-        pass
+    def sleep(self) -> None:
+        print(self.name, "Se ha dormido")
 
 
-    def eat() -> None:
-        pass
+    def eat(self, user_food) -> None:
+        print(self.name, "Ha comido:", user_food.pet_food)
 
 
-    def play() -> None:
-        pass
+    def play(self) -> None:
+        print(self.name, "Ha rodado sobre si mismo")
 
 
-    def noise() -> None:
-        pass
+    def noise(self) -> None:
+        print(self.name, "Ha hecho un sonido")
+
+class Cat(Pets):
+    def __init__(self, name, type, treats, health, energy) -> None:
+        super().__init__(name, type, treats, health, energy)
+
+    def noise(self) -> None:
+        print("Meow!")
+
+class Cat(Pets):
+    def __init__(self, name, treats, health, energy) -> None:
+        super().__init__(name, "High quality cat", treats, health, energy)
+
+    def noise(self) -> None:
+        print("Meow!")
