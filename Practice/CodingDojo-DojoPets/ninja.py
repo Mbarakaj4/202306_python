619d9a000df2c3c0668148e31fe22dc81208dff3
Mbarakaj4 <113691935+Mbarakaj4@users.noreply.github.com>
2023-07-18 21:07:55 -0400

    Add DojoPets
    Creating ninja and pet classes

---
+++ Practice/CodingDojo-DojoPets/ninja.py
@@ -0,0 +1,19 @@
+class Ninja:
+    def __init__(self, name, lastname, treats, pet_food, pet) -> None:
+        self.name = name
+        self.lastname = lastname
+        self.treats = treats
+        self.pet_food = pet_food
+        self.pet = pet
+
+
+    def walk() -> None:
+        pass
+
+
+    def feed() -> None:
+        pass
+
+
+    def shower() -> None:
+        pass
