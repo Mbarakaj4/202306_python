e98c3bfa61f4db31f806f18f427a528d39056edb
Mbarakaj4 <113691935+Mbarakaj4@users.noreply.github.com>
2023-07-20 01:25:00 -0400

    Add Shop

---
+++ Optional/CodingDojo-Shop/ShopProduct.py
@@ -0,0 +1,11 @@
+"""
+    Shop and products
+    Create a shop class that will have 2 attributes; a name and a list
+    of products, this will start empty.
+
+    Then create a product class that will have 3 attributes; name price
+    and a category
+"""
+import Shop
+import Product
+
